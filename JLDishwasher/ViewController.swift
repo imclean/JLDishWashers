7d5c40de7ee7ccf0100975e43a48f69e886203f0
Iain McLean <iain@brightskyapps.com>
2016-12-21 13:41:12 +0000

    Initial Commit

---
+++ JLDishwasher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JLDishwasher
+//
+//  Created by Iain McLean on 21/12/2016.
+//  Copyright © 2016 Iain McLean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
